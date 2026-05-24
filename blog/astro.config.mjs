f3a7c5dae17287e86e6a9690294049ed6f963232
nana <hannaoh.us@gmail.com>
2026-05-24 19:17:57 +0900

    fix: remove sitemap (v3 bug) - blog/astro.config.mjs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
@@ -10,7 +9,6 @@ export default defineConfig({
   site: 'https://blog.krk.team',
   integrations: [
     mdx(),
-    sitemap(),
   ],
   markdown: {
     shikiConfig: { theme: 'github-light' },
